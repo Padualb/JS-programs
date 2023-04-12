22a99d3e6924af76566560b5830533f2ec53e210
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2023-04-11 23:54:13 -0300

    API connection

--- Weather/js/script.js
+++ Weather/js/script.js
@@ -1,1 +1,10 @@
 let weatherAPIkey = 'b55002c98650706b2d6e3c1d6907a2ea';
+let weatherBaseEndpoint = 'https://api.openweathermap.org/data/2.5/weather?appid=' + weatherAPIkey;
+let getWeatherByCityName = async(city) => {
+    let endpoint = weatherBaseEndpoint + '&q=' + city;
+    let response = await fetch(endpoint);
+    console.log(response);
+    
+}
+
+getWeatherByCityName('New York');
